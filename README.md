20772131eb8b4b1ec44b6ce28381bfb46b33dacd
Manas <manas18244@iiitd.ac.in>
2021-10-09 16:11:56 +0530

    Add boilerplate for the simulator
    It contains a (build-able) dummy code. README contains build
    instructions.

---
+++ README.md
@@ -0,0 +1,14 @@
+# RISC-V Simulator
+
+This project aims to simulate a certain subset of RISC-V ISA.
+
+## Building
+
+Run the following commands in the root directory of the project:
+
+```bash
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
---
+++ src/Driver.cpp
@@ -0,0 +1,14 @@
+/* It contains the main driver's code for the simulator.
+ *
+ */
+#include "Driver.hpp"
+
+int main(int argc, char **argv) {
+  using namespace std;
+  cout << "Initialized the simulator\n";
+
+  while (true) {
+  }
+
+  return 0;
+}
